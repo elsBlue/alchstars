5f48039117a7cb376c3b54e4e333b87f3f53176e
Iqbal Hasan <31207626+elsBlue@users.noreply.github.com>
2021-06-22 20:56:12 +0700

    Update dt.js

--- js/dt.js
+++ js/dt.js
@@ -184,7 +184,426 @@ var Engine = {
 			]
 		},
 
+		{
+			"name" 		: "Saleucid",
+			"world" 	: "Umbraton",
+			"attribute" : "Water",
+			"rarity"	: "4",	
+			"figure" 	: "saleucid/figure.jpg",
+			"category" 	: [
+				{"name" : "Detonator"},
+			],
 
+			"skill" 	: [
+				{	
+					"type" 			: "Active",
+					"name" 			: "Roar of Fury",
+					"img" 			: "saleucid/skill1.jpg",
+					"description" 	: "Deals 200% damage to 1 surrounding cluster.",
+				},
+
+				{	
+					"type" 			: "Chain Combo",
+					"name" 			: "Leaping Cleave",
+					"img" 			: "saleucid/skill2.jpg",
+					"description" 	: "Deals 150% damage to 1 surrounding cluster.",
+				},
+				
+				{	
+					"type" 			: "Equipment Skills",
+					"name" 			: "Crushing Force",
+					"img" 			: "saleucid/skill3.jpg",
+					"description" 	: "There is a 30% chance of increasing Chain Combo damage by 50%.",
+				}
+					
+			]
+		},
+
+		{
+			"name" 		: "Sariel",
+			"world" 	: "Umbraton",
+			"attribute" : "Water",
+			"rarity"	: "6",	
+			"figure" 	: "sariel/figure.jpg",
+			"category" 	: [
+				{"name" : "Converter"},
+			],
+
+			"skill" 	: [
+				{	
+					"type" 			: "Active",
+					"name" 			: "Turbid Wheel",
+					"img" 			: "sariel/skill1.jpg",
+					"description" 	: "Converts the 4 nearest Red or Green tiles to Blue.",
+				},
+
+				{	
+					"type" 			: "Chain Combo",
+					"name" 			: "Tidebreaker",
+					"img" 			: "sariel/skill2.jpg",
+					"description" 	: "Deals 150% damage 3 times. assigned randomly to enemies withing 2 surronding clusters of this unit. When a target takes damage more than once. halve the damage.",
+				},
+				
+				{	
+					"type" 			: "Equipment Skills",
+					"name" 			: "Drizzle Curtain",
+					"img" 			: "sariel/skill3.jpg",
+					"description" 	: "Grants a Sariel Mark, up to 8 stacks. Each stack increase Sariel's DEF by 6%. Retains 50% the stack at the end of the round.",
+				}
+					
+			]
+		},
+
+		{
+			"name" 		: "Sork & Bekk",
+			"world" 	: "Rediesel Wrench",
+			"attribute" : "Fire",
+			"rarity"	: "4",	
+			"figure" 	: "sork/figure.jpg",
+			"category" 	: [
+				{"name" : "Detonator"},
+			],
+
+			"skill" 	: [
+				{	
+					"type" 			: "Active",
+					"name" 			: "M23 High Explosive",
+					"img" 			: "sork/skill1.jpg",
+					"description" 	: "Attacks in any 1 selected direction. Deals 240% damage to the first enemy hit and to enemies within 1 surrounding circle.",
+				},
+
+				{	
+					"type" 			: "Chain Combo",
+					"name" 			: "Ragging Bekk",
+					"img" 			: "sork/skill2.jpg",
+					"description" 	: "Deals 140% damage to 12 tiles in a diamond shape.",
+				},
+				
+				{	
+					"type" 			: "Equipment Skills",
+					"name" 			: "Bombardment",
+					"img" 			: "sork/skill3.jpg",
+					"description" 	: "Increases Active Skill damage by 20%.",
+				}
+					
+			]
+		},
+
+		{
+			"name" 		: "Tessa",
+			"world" 	: "Umbraton",
+			"attribute" : "Thunder",
+			"rarity"	: "4",	
+			"figure" 	: "tessa/figure.jpg",
+			"category" 	: [
+				{"name" : "Detonator"},
+			],
+
+			"skill" 	: [
+				{	
+					"type" 			: "Active",
+					"name" 			: "Electric Current",
+					"img" 			: "tessa/skill1.jpg",
+					"description" 	: "Selects 1 of 4 directions around the unit and pulls over all enemies in the selected direction and the adjacent rows and columns to the unit, dealing 180% damage to the enemies that are pulled over.",
+				},
+
+				{	
+					"type" 			: "Chain Combo",
+					"name" 			: "Power Surge",
+					"img" 			: "tessa/skill2.jpg",
+					"description" 	: "Deals 150% damage to 1 surrounding cluster.",
+				},
+				
+				{	
+					"type" 			: "Equipment Skills",
+					"name" 			: "Plasma Point",
+					"img" 			: "tessa/skill3.jpg",
+					"description" 	: "Grants a Tessa Mark. When using an Active Skill, each mark deals 20% damage to enemies within 1 surrounding cluster around Tessa. Activing the Active Skills clears all marks.",
+				}
+					
+			]
+		},
+
+		{
+			"name" 		: "Tiny One",
+			"world" 	: "Rediesel Wrench",
+			"attribute" : "Fire",
+			"rarity"	: "4",	
+			"figure" 	: "tiny/figure.jpg",
+			"category" 	: [
+				{"name" : "Converter"},
+			],
+
+			"skill" 	: [
+				{	
+					"type" 			: "Active",
+					"name" 			: "Encore",
+					"img" 			: "tiny/skill1.jpg",
+					"description" 	: "Converts 1 traversable tile to 1 Red Prism tile.",
+				},
+
+				{	
+					"type" 			: "Chain Combo",
+					"name" 			: "Intense Glare",
+					"img" 			: "tiny/skill2.jpg",
+					"description" 	: "Deals 150% damage to 1 surrounding cluster.",
+				},
+				
+				{	
+					"type" 			: "Equipment Skills",
+					"name" 			: "Spotlight",
+					"img" 			: "tiny/skill3.jpg",
+					"description" 	: "Tiny One's Normal Attack damage increases by 20% for this round after using Active Skill.",
+				}
+					
+			]
+		},
+
+		{
+			"name" 		: "Unimet",
+			"world" 	: "Illumina Federation",
+			"attribute" : "Thunder",
+			"rarity"	: "3",	
+			"figure" 	: "unimet/figure.jpg",
+			"category" 	: [
+				{"name" : "Converter"},
+			],
+
+			"skill" 	: [
+				{	
+					"type" 			: "Active",
+					"name" 			: "Universal Support Box",
+					"img" 			: "unimet/skill1.jpg",
+					"description" 	: "Throws a can at any selected tile, changing the tile to a Yellow tile. A Lightning Crystal, Meteorite Crystal, Bandage, Attack, or Defense is randomly generated on the tile.",
+				},
+
+				{	
+					"type" 			: "Chain Combo",
+					"name" 			: "Universal Mechanical Fist",
+					"img" 			: "unimet/skill2.jpg",
+					"description" 	: "Deals 150% damage to 1 surrounding cluster.",
+				},
+				
+				{	
+					"type" 			: "Equipment Skills",
+					"name" 			: "Machine Fanatic",
+					"img" 			: "unimet/skill3.jpg",
+					"description" 	: "When there are no enemies within the Chain Combo range, recovers the team's HP equal to 80% of Unimet's ATK.",
+				}
+					
+			]
+		},
+
+
+		{
+			"name" 		: "Uriah",
+			"world" 	: "Lumopolis",
+			"attribute" : "Forest",
+			"rarity"	: "4",	
+			"figure" 	: "uriah/figure.jpg",
+			"category" 	: [
+				{"name" : "Support"},
+			],
+
+			"skill" 	: [
+				{	
+					"type" 			: "Active",
+					"name" 			: "Vines of Prayer",
+					"img" 			: "uriah/skill1.jpg",
+					"description" 	: "Recovers the team's HP by 60% of Uriah's Max HP. If there is any excess healing, the nearest Green tiles is converted to and enhanced tile.",
+				},
+
+				{	
+					"type" 			: "Chain Combo",
+					"name" 			: "Prayer of Healing",
+					"img" 			: "uriah/skill2.jpg",
+					"description" 	: "Recovers the team's HP by 160% of Uriah's ATK.",
+				},
+				
+				{	
+					"type" 			: "Equipment Skills",
+					"name" 			: "Eulogy",
+					"img" 			: "uriah/skill3.jpg",
+					"description" 	: "Recovers HP equal to 40% of Uriah's DEF when passing through an enhanced Green tile.",
+				}
+					
+			]
+		},
+
+		{
+			"name" 		: "Vice",
+			"world" 	: "Illumina Federation",
+			"attribute" : "Water",
+			"rarity"	: "5",	
+			"figure" 	: "vice/figure.jpg",
+			"category" 	: [
+				{"name" : "Sniper"},
+			],
+
+			"skill" 	: [
+				{	
+					"type" 			: "Active",
+					"name" 			: "Rapid Torrent",
+					"img" 			: "vice/skill1.jpg",
+					"description" 	: "Deals 80% damage 5 times to random enemies on the field. Launches 1 additional attack for each Water tile within 1 surrounding cluster of the unit.",
+				},
+
+				{	
+					"type" 			: "Chain Combo",
+					"name" 			: "Homing Arrows",
+					"img" 			: "vice/skill2.jpg",
+					"description" 	: "Deals 140% damage to the 2 nearest enemies",
+				},
+				
+				{	
+					"type" 			: "Equipment Skills",
+					"name" 			: "Target Lock",
+					"img" 			: "vice/skill3.jpg",
+					"description" 	: "Vice gains 1 stack of Locking Marks whenever she deals damage. Each stack increases damage by 1%. All stacks are cleared at the end of the round.",
+				}
+					
+			]
+		},
+
+		{
+			"name" 		: "Wendy",
+			"world" 	: "Independent",
+			"attribute" : "Forest",
+			"rarity"	: "4",	
+			"figure" 	: "tiny/figure.jpg",
+			"category" 	: [
+				{"name" : "Converter"},
+			],
+
+			"skill" 	: [
+				{	
+					"type" 			: "Active",
+					"name" 			: "Nimble Hurricane",
+					"img" 			: "wendy/skill1.jpg",
+					"description" 	: "Transports to any location and generates 1 Wendy's Feather on the spot. Tap Wendy's Feather to return to the original location. There can only 1 Wendy's Feather on battlefield.",
+				},
+
+				{	
+					"type" 			: "Chain Combo",
+					"name" 			: "Owl Kick",
+					"img" 			: "wendy/skill2.jpg",
+					"description" 	: "Deals 150% damage to 8 tiles in a cross shape.",
+				},
+				
+				{	
+					"type" 			: "Equipment Skills",
+					"name" 			: "Windrider",
+					"img" 			: "wendy/skill3.jpg",
+					"description" 	: "When an Aurorian teleports. Wendy gains 1 stack of Mark for each cluster between the Aurorian's initial location and their teleport destination. Each stack increases the damage of Wendy's next normal attack by 1% Launching normal attacks clears the Mark.",
+				}
+					
+			]
+		},
+
+		{
+			"name" 		: "White Dwarf",
+			"world" 	: "Illumina Federation",
+			"attribute" : "Water",
+			"rarity"	: "3",	
+			"figure" 	: "white/figure.jpg",
+			"category" 	: [
+				{"name" : "Support"},
+			],
+
+			"skill" 	: [
+				{	
+					"type" 			: "Active",
+					"name" 			: "Gravity Ruins",
+					"img" 			: "white/skill1.jpg",
+					"description" 	: "Deals 250% damage to enemies in a Cross shape to the maximum range and aggros them to the unit.",
+				},
+
+				{	
+					"type" 			: "Chain Combo",
+					"name" 			: "Gravity Ash",
+					"img" 			: "white/skill2.jpg",
+					"description" 	: "Deals 150% damage to 8 tiles in a cross shape.",
+				},
+				
+				{	
+					"type" 			: "Equipment Skills",
+					"name" 			: "Gravitation Meld",
+					"img" 			: "white/skill3.jpg",
+					"description" 	: "Increases Active Skill damage by 20%.",
+				}
+					
+			]
+		},
+
+		{
+			"name" 		: "Wrath",
+			"world" 	: "Illumina Federation",
+			"attribute" : "Thunder",
+			"rarity"	: "6",	
+			"figure" 	: "wrath/figure.jpg",
+			"category" 	: [
+				{"name" : "Sniper"},
+			],
+
+			"skill" 	: [
+				{	
+					"type" 			: "Active",
+					"name" 			: "Overload Mode",
+					"img" 			: "wrath/skill1.jpg",
+					"description" 	: "Enters the Overcharge state in which Wrath's Chain Combo deals damage to the nearest enemy 2 times for 2 rounds.",
+				},
+
+				{	
+					"type" 			: "Chain Combo",
+					"name" 			: "Patrol Attack",
+					"img" 			: "wrath/skill2.jpg",
+					"description" 	: "Deals 110% damage to the nearest enemy and 30% splash damage to enemies in 4 tiles in a cross shape around it.",
+				},
+				
+				{	
+					"type" 			: "Equipment Skills",
+					"name" 			: "Patrol Array",
+					"img" 			: "wrath/skill3.jpg",
+					"description" 	: "Bullets can be reserved. When a Chain Combo deals damage, consumes 1 bullet and increases the final damage by 15%",
+				}
+					
+			]
+		},
+
+		{
+			"name" 		: "Zoya",
+			"world" 	: "Illumina Federation",
+			"attribute" : "Water",
+			"rarity"	: "3",	
+			"figure" 	: "zoya/figure.jpg",
+			"category" 	: [
+				{"name" : "Support"},
+			],
+
+			"skill" 	: [
+				{	
+					"type" 			: "Active",
+					"name" 			: "Anticipation of Love",
+					"img" 			: "water/skill1.jpg",
+					"description" 	: "recovers the team's HP by 150% of Zoya's ATK. Each Blue tile on the field increases healing effectiveness by 5%.",
+				},
+
+				{	
+					"type" 			: "Chain Combo",
+					"name" 			: "Nether Sea",
+					"img" 			: "zoya/skill2.jpg",
+					"description" 	: "Recovers the team's HP by 160% of Zoya's ATK.",
+				},
+				
+				{	
+					"type" 			: "Equipment Skills",
+					"name" 			: "Field-Zero Snow",
+					"img" 			: "zoya/skill3.jpg",
+					"description" 	: "Increases Chain Combo Healing Factor by 10%.",
+				}
+					
+			]
+		},
 
 	]
 }
