8a69b016b5d7ba92841b30d4d8badff98d6f1910
Iqbal Hasan <31207626+elsBlue@users.noreply.github.com>
2021-06-25 03:51:19 +0700

    a

--- js/ui.js
+++ js/ui.js
@@ -16,6 +16,48 @@ $(document).on("click", ".rear-info", function(){
 	}
 });
 
+$(document).on("click", ".expand-all", function(){
+	var a = $(".rear-info").siblings();
+	var b = $(".rear-info").children(".indicator").children(".down");
+	var c = $(".rear-info").children(".indicator").children(".up");
+
+	
+		a.slideDown( 400 );
+		a.addClass("active");
+		c.show();
+		b.hide();
+});
+
+$(document).on("click", ".shrink-all", function(){
+	var a = $(".rear-info").siblings();
+	var b = $(".rear-info").children(".indicator").children(".down");
+	var c = $(".rear-info").children(".indicator").children(".up");
+
+	
+		a.slideUp( 400 );
+		a.removeClass("active");
+		b.show();
+		c.hide();
+});
+
+$(document).on("click", ".jump", function(){
+		var a = $(".mini-characterlist");
+		a.show();
+		$(this).removeClass("jump");
+		$(this).addClass("jump-out");
+		$(this).css("background-color","#FFD700");
+		$(".holder").html("<span class='material-icons'>arrow_drop_down</span>");
+});
+
+$(document).on("click", ".jump-out", function(){
+		var a = $(".mini-characterlist");
+		a.hide();
+		$(this).removeClass("jump-out");
+		$(this).addClass("jump");
+		$(this).css("background-color","#FFF");
+		$(".holder").html("Jump");
+});
+
 function searchChar() {
   
   var input, filter, ul, li, a, i, txtValue;
@@ -35,3 +77,28 @@ function searchChar() {
     }
   }
 }
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 500, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
